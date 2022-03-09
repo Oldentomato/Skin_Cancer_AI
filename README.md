40644b8ae02b66a3d49995fd7252ac8c47c2bd80
Oldentomato <ender1412@naver.com>
2022-03-09 18:31:06 +0900

    코드추가, 마크다운내용 수정

--- README.md
+++ README.md
@@ -6,8 +6,6 @@
   
 * * *
 
-=======
-**트레이닝이미지: 22,000장 검증이미지: 11,000장 테스트이미지: 40,000장**
 
 > 첫번째 학습 결과  
 
@@ -42,3 +40,48 @@ Non-trainable params: 0
  ![loss](https://github.com/Oldentomato/Skin_Cancer_AI/blob/main/Images/loss.png?raw=true)
 
 
+
+> 두번째 학습 결과  
+
+**트레이닝이미지: 2,463장 검증이미지: 1,196장**  
+* 모델 구성
+Model: "sequential_9"
+_________________________________________________________________
+ Layer (type)                Output Shape              Param #   
+
+ conv2d_25 (Conv2D)          (None, 28, 28, 16)        160       
+                                                                 
+ max_pooling2d_25 (MaxPoolin  (None, 14, 14, 16)       0         
+ g2D)                                                            
+                                                                 
+ conv2d_26 (Conv2D)          (None, 14, 14, 16)        2320      
+                                                                 
+ max_pooling2d_26 (MaxPoolin  (None, 7, 7, 16)         0         
+ g2D)                                                            
+                                                                 
+ conv2d_27 (Conv2D)          (None, 7, 7, 16)          2320      
+                                                                 
+ max_pooling2d_27 (MaxPoolin  (None, 3, 3, 16)         0         
+ g2D)                                                            
+                                                                 
+ flatten_9 (Flatten)         (None, 144)               0         
+                                                                 
+ dense_25 (Dense)            (None, 128)               18560     
+                                                                 
+ dropout_16 (Dropout)        (None, 128)               0         
+                                                                 
+ dense_26 (Dense)            (None, 32)                4128      
+                                                                 
+ dropout_17 (Dropout)        (None, 32)                0         
+                                                                 
+ dense_27 (Dense)            (None, 1)                 33        
+                                                                 
+Total params: 27,521
+Trainable params: 27,521
+Non-trainable params: 0
+_________________________________________________________________  
+
+* 정확도 그래프  
+ ![accuracy](https://github.com/Oldentomato/Skin_Cancer_AI/blob/main/Images/accuracy_2.png?raw=true)
+* 손실 그래프  
+ ![loss](https://github.com/Oldentomato/Skin_Cancer_AI/blob/main/Images/loss_2.png?raw=true)
---
+++ Skin_Cancer_CNN2.py
@@ -0,0 +1,120 @@
+#In[1]
+import tensorflow as tf
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+    try:
+        # Currently, memory growth needs to be the same across GPUs
+        for gpu in gpus:
+            tf.config.experimental.set_memory_growth(gpu, True)
+        logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+        print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+    except RuntimeError as e:
+        # Memory growth must be set before GPUs have been initialized
+        print(e)
+
+#In[2]
+import pandas as pd
+import os
+import cv2
+import shutil
+
+directory = 'C:/Users/COMPUTER/Desktop/skin_cancer_images'
+files = os.listdir(directory+'/train_encoded')
+df = pd.read_csv('C:/Users/COMPUTER/Desktop/skin_cancer/ISIC_2020_Training_GroundTruth.csv')
+df = df.drop(0, axis=0)
+condition_be = (df.target == 0)
+condition_mal = (df.target == 1)
+benign = df.loc[:][condition_be]
+malignant = df.loc[:][condition_mal]
+benign.reset_index(inplace=True)
+malignant.reset_index(inplace=True)
+
+
+
+for i in range(0,len(benign)):
+    shutil.move(directory + '/train_encoded/'+benign['image_name'][i]+'.jpg', directory + '/benign/'+benign['image_name'][i]+'.jpg')
+    
+for i in range(0,len(malignant)):
+    shutil.move(directory + '/train_encoded/'+malignant['image_name'][i]+'.jpg', directory + '/malignant/'+malignant['image_name'][i]+'.jpg')
+
+#In[3]
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+train_datagen = ImageDataGenerator(
+    rescale = 1. /255
+)
+valid_test_datagen = ImageDataGenerator(
+    rescale = 1. /255
+)
+
+train_generator = train_datagen.flow_from_directory(
+    directory+'/train',
+    target_size = (28,28),
+    color_mode = 'grayscale',
+    class_mode = 'binary',
+    shuffle = True,
+    batch_size = 32,
+)
+
+valid_generator = valid_test_datagen.flow_from_directory(
+    directory+'/test',
+    target_size = (28,28),
+    color_mode = 'grayscale',
+    class_mode = 'binary',
+    shuffle = True,
+    batch_size = 32,
+)
+
+
+#In[4]
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
+import tensorflow as tf
+from tensorflow.keras.layers import Dropout
+
+conv1 = tf.keras.Sequential()
+conv1.add(Conv2D(16,(3,3),activation='relu', padding='same',input_shape=(28,28,1)))
+conv1.add(MaxPooling2D(2,2))
+conv1.add(Conv2D(16,(3,3),activation='relu', padding='same'))
+conv1.add(MaxPooling2D(2,2))
+conv1.add(Conv2D(16,(3,3),activation='relu', padding='same'))
+conv1.add(MaxPooling2D(2,2))
+
+conv1.add(Flatten())
+
+
+
+conv1.add(Dense(128,activation='relu'))
+conv1.add(Dropout(0.5))
+conv1.add(Dense(32,activation='relu'))
+conv1.add(Dropout(0.5))
+conv1.add(Dense(1,activation='sigmoid'))
+
+conv1.summary()
+
+#In[5]
+conv1.compile(optimizer='adam',loss='binary_crossentropy',metrics=['accuracy'])
+tf.debugging.set_log_device_placement(True)
+
+with tf.device("/gpu:0"):
+    history = conv1.fit_generator(train_generator,
+                                 #steps_per_epoch=20,
+                                 epochs=100,
+                                 validation_data=valid_generator,
+                                 #validation_steps=10,
+                                 shuffle=True)
+
+#In[6]
+import matplotlib.pyplot as plt
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train_loss','val_loss'])
+plt.show()
+
+plt.plot(history.history['accuracy'])
+plt.plot(history.history['val_accuracy'])
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train_accuracy','val_accuracy'])
+plt.show()
